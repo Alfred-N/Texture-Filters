6193411cf2f42a0b2595062884c8f3dd25c66bd7
Alfred-N <alfredn@kth.se>
2023-03-23 02:57:29 +0100

    add faiss kmeans engine, much faster

--- kmeans_filter.py
+++ kmeans_filter.py
@@ -2,19 +2,98 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import time
 from sklearn.cluster import KMeans
+import faiss
 
 LUMINANCE_COEFFS = np.array([0.2126, 0.7152, 0.0722])
 
 
-def get_topk_colors_kmeans(img_np, k, num_clusters=100, random_state=42):
-    img_pixels = img_np.reshape(-1, 3)
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(
+            f"Function {func.__name__} took {elapsed_time:.6f} seconds ({elapsed_time*1000:.6f} milliseconds)"
+        )
+        return result
 
+    return wrapper
+
+
+def kmeans_sklearn(
+    img_pixels, k, num_clusters=100, n_init=1, max_iter=1, random_state=42
+):
     kmeans = KMeans(
-        n_clusters=num_clusters, random_state=random_state, n_init="auto"
+        n_clusters=num_clusters,
+        random_state=random_state,
+        n_init=n_init,
+        max_iter=max_iter,
     ).fit(img_pixels)
     top_colors = kmeans.cluster_centers_
     return top_colors[:k]
+
+
+def kmeans_faiss(
+    img_pixels, k, num_clusters=100, n_init=1, max_iter=1, gpu=False, random_state=42
+):
+    d = img_pixels.shape[1]
+    kmeans = faiss.Kmeans(
+        d,
+        num_clusters,
+        niter=max_iter,
+        nredo=n_init,
+        verbose=True,
+        gpu=gpu,
+        seed=random_state,
+    )
+
+    # Remove default behaviour of subsampling the training data
+    if gpu:
+        kmeans.max_points_per_centroid = 1000000
+    else:
+        kmeans.cp.max_points_per_centroid = 1000000
+
+    kmeans.train(img_pixels.astype(np.float32))
+    centroids = kmeans.centroids
+
+    # Assign each instance to a centroid
+    D, I = kmeans.index.search(img_pixels.astype(np.float32), 1)
+
+    # Count the number of instances belonging to each centroid
+    counts = np.bincount(I.reshape(-1), minlength=num_clusters)
+    centroids_sorted = centroids[np.argsort(-counts)]
+    # top_colors = centroids[:k]
+    top_colors = centroids_sorted[:k]
+    return top_colors
+
+
+# @time_it
+def get_topk_colors_kmeans(
+    img_np,
+    k,
+    engine="sklearn",
+    gpu=False,
+    num_clusters=100,
+    n_init=1,
+    max_iter=1,
+    random_state=42,
+):
+    img_pixels = img_np.reshape(-1, 3)
+
+    if engine == "sklearn":
+        top_colors = kmeans_sklearn(
+            img_pixels, k, num_clusters, n_init, max_iter, random_state
+        )
+    elif engine == "faiss":
+        top_colors = kmeans_faiss(
+            img_pixels, k, num_clusters, n_init, max_iter, gpu, random_state
+        )
+    else:
+        raise NotImplementedError('Invalid kmeans engine: choose "sklearn"/"faiss"')
+    return top_colors
 
 
 def round_img_to_colors(img_np, colors_arr):
@@ -139,9 +218,7 @@ def plot_filtered_and_original(
     # Plot the color palette below the images
     ax4 = fig.add_subplot(gs[1, :])
     ax4.set_title(f"Top {k} Colors")
-    ax4.imshow(
-        color_palette[:, :, ::-1].astype(np.uint8), aspect=1/5
-    )
+    ax4.imshow(color_palette[:, :, ::-1].astype(np.uint8), aspect=1 / 5)
     ax4.set_axis_off()
     plt.tight_layout()
     plt.savefig(savepath)
@@ -157,7 +234,13 @@ def main(
     random_state=42,
     plot_comparison=False,
     luminance_correction=True,
+    engine="sklearn",
+    gpu=False,
+    n_init=1,
 ):
+    # Sanity checks
+    assert not (gpu and engine == "sklearn"), "Only faiss is compatible with gpu"
+
     # Read the image using OpenCV
     img = cv2.imread(input_path)
     img_np = np.array(img)
@@ -169,13 +252,25 @@ def main(
 
     print("K-means clustering input image ...")
     top_k_colors = get_topk_colors_kmeans(
-        img_np, n_colors, num_clusters=num_clusters, random_state=random_state
+        img_np,
+        n_colors,
+        num_clusters=num_clusters,
+        random_state=random_state,
+        engine=engine,
+        gpu=gpu,
+        n_init=n_init,
     )
     result_np, nearest_colors_inds_orig = round_img_to_colors(img, top_k_colors)
     if ref_path is not None:
         print("K-means clustering reference image ...")
         top_k_colors_ref = get_topk_colors_kmeans(
-            ref_np, n_colors, num_clusters=num_clusters, random_state=random_state
+            ref_np,
+            n_colors,
+            num_clusters=num_clusters,
+            random_state=random_state,
+            engine=engine,
+            gpu=gpu,
+            n_init=n_init,
         )
         result_np = swap_colors(
             nearest_colors_inds_orig,
@@ -247,6 +342,20 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--n_random",
+        type=int,
+        default=1,
+        help="Number of different random seeds to start from for k-means clustering (increases time linearly).",
+    )
+
+    parser.add_argument(
+        "--max_iter",
+        type=int,
+        default=1,
+        help="Number of iterations to use for k-means clustering.",
+    )
+
+    parser.add_argument(
         "--downsample_ratio",
         type=int,
         default=1,
@@ -273,6 +382,19 @@ if __name__ == "__main__":
         help="Disable luminance correction when swapping colors.",
     )
 
+    parser.add_argument(
+        "--engine",
+        choices=["faiss", "sklearn"],
+        default="faiss",
+        help="The clustering engine to use",
+    )
+
+    parser.add_argument(
+        "--gpu",
+        action="store_true",
+        help='Use gpu for kmeans clustering. Only compatible with engine="faiss"',
+    )
+
     args = parser.parse_args()
 
     main(
@@ -285,4 +407,7 @@ if __name__ == "__main__":
         random_state=args.random_state,
         plot_comparison=args.plot_comparison,
         luminance_correction=args.luminance_correction,
+        engine=args.engine,
+        gpu=args.gpu,
+        n_init=args.n_random,
     )
